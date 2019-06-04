11b0746a6ec2bc36c9e367030d2822acd27f4735
Depauw Natan <natan.depauw@telenetgroup.be>
2019-06-04 16:25:02 +0200

    added version

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ img_name=$1
 dockerhub_username=$2
 
 cd "$dockerfilepath"/"$img_name" || return
+version=$(cat version)
 
+docker build . -t "${dockerhub_username}"/"${img_name}":"${version}"
 docker build . -t "${dockerhub_username}"/"${img_name}":latest
--- push.sh
+++ push.sh
@@ -7,6 +7,10 @@ dockerhub_username=$2
 docker_username=$3
 docker_password=$4
 
+cd "$dockerfilepath"/"$img_name" || return
+version=$(cat version)
+
 docker login -u $docker_username -p $docker_password
+docker push "${dockerhub_username}"/"${img_name}":"${version}"
 docker push "${dockerhub_username}"/"${img_name}":latest
 docker logout
